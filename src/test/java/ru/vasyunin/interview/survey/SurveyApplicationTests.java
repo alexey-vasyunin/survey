daec2df58febcc4f1ee27d2c36c7fd247faf6530
Alexey Vasyunin <a.vasyunin@gmail.com>
2019-11-11 17:34:29 +0400

    Initial commit

---
+++ src/test/java/ru/vasyunin/interview/survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasyunin.interview.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
