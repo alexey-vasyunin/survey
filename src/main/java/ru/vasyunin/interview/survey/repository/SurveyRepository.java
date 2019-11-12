b90df9b204d09bc3e15a482c7aa43f65cc44cd07
Alexey Vasyunin <a.vasyunin@gmail.com>
2019-11-12 16:36:37 +0400

    Контроллер и сервис для получения/создания/редактирования/удаления опросов

--- src/main/java/ru/vasyunin/interview/survey/repository/SurveyRepository.java
+++ src/main/java/ru/vasyunin/interview/survey/repository/SurveyRepository.java
@@ -3,8 +3,12 @@ package ru.vasyunin.interview.survey.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.vasyunin.interview.survey.dto.SurveyDto;
 import ru.vasyunin.interview.survey.entity.Survey;
+
+import java.util.List;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
+    List<SurveyDto> findAllBy(Pageable page);
 }
